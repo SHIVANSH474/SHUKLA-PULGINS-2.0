a5eb1bce8bb1bb32fa4da8127154bfd61b8caa3e
BAD BABY <154504921+Badhacker98@users.noreply.github.com>
2024-01-03 18:00:22 +0530

    Update clients.py

--- Hellbot/core/clients.py
+++ Hellbot/core/clients.py
@@ -127,10 +127,10 @@ class HellClient(Client):
                 [
                     [
                         InlineKeyboardButton("💫 Start Me", url=f"https://t.me/{self.bot.me.username}?start=start"),
-                        InlineKeyboardButton("💖 Repo", url="https://github.com/badkopapabolo"),
+                        InlineKeyboardButton("💖 Repo", url="https://t.me/ll_THE_BAD_BOT_ll"),
                     ],
                     [
-                        InlineKeyboardButton("🦋 𝐏ʙx 𝐁ᴏᴛ 𝐍ᴇᴛᴡᴏʀᴋ ❤️", url="https://t.me/II_BAD_MUNDA_II"),
+                        InlineKeyboardButton("🦋 𝐏ʙx 𝐁ᴏᴛ 𝐍ᴇᴛᴡᴏʀᴋ ❤️", url="https://t.me/ll_THE_BAD_BOT_ll"),
                     ],
                 ]
             ),
