dcae4b1b0011a7cc1ab5c7f90b65002b4736317b
BAD BABY <154504921+Badhacker98@users.noreply.github.com>
2024-01-04 10:41:25 +0530

    Update pmpermit.py

--- Hellbot/plugins/user/pmpermit.py
+++ Hellbot/plugins/user/pmpermit.py
@@ -200,7 +200,7 @@ async def handle_incoming_pm(client: Client, message: Message):
             f"**{Symbols.cross_mark} 𝖤𝗇𝗈𝗎𝗀𝗁 𝗈𝖿 𝗒𝗈𝗎𝗋 𝗌𝗉𝖺𝗆𝗆𝗂𝗇𝗀 𝗁𝖾𝗋𝖾! 𝖡𝗅𝗈𝖼𝗄𝗂𝗇𝗀 𝗒𝗈𝗎 𝖿𝗋𝗈𝗆 𝖯𝖬 𝗎𝗇𝗍𝗂𝗅 𝖿𝗎𝗋𝗍𝗁𝖾𝗋 𝗇𝗈𝗍𝗂𝖼𝖾.**",
         )
 
-    pm_msg = f"🍀 𝐇𝐞𝐥𝐥𝐁𝐨𝐭 𝐏𝐌 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲!\n\n"
+    pm_msg = f"🍀 𝐏ʙx 𝐁ᴏᴛ 2.0  𝐏𝐌 𝐒𝐞𝐜𝐮𝐫𝐢𝐭𝐲!\n\n"
     custom_pmmsg = await db.get_env(ENV.custom_pmpermit)
 
     if custom_pmmsg:
@@ -242,23 +242,23 @@ HelpMenu("pmpermit").add(
     "block",
     "<reply to user>/<userid/username>",
     "Block a user from pm-ing you.",
-    "block @ForGo10God",
+    "block @ll_THE_BAD_BOT_ll",
 ).add(
     "unblock",
     "<reply to user>/<userid/username>",
     "Unblock a user from pm-ing you.",
-    "unblock @ForGo10God",
+    "unblock @ll_THE_BAD_BOT_ll",
 ).add(
     "allow",
     "<reply to user>/<userid/username>",
     "Allow a user to pm you.",
-    "allow @ForGo10God",
+    "allow @ll_THE_BAD_BOT_ll",
     "An alias of 'approve' is also available.",
 ).add(
     "disallow",
     "<reply to user>/<userid/username>",
     "Disallow a user to pm you.",
-    "disallow @ForGo10God",
+    "disallow @ll_THE_BAD_BOT_ll",
     "An alias of 'disapprove' is also available.",
 ).add(
     "allowlist",
