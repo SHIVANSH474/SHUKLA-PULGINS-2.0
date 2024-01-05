3c9245321dd4d3315bbfce7b1def562ae6c4805a
BAD BABY <154504921+Badhacker98@users.noreply.github.com>
2024-01-05 16:10:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ echo "  | |_) || |__| |   | |     "
 echo "  ||____/  \____/   |_|     "
 echo "                                                            "
 
-python3 -m Hellbot                                                               
+python3 -m PBXBOT 2.0                                                               
