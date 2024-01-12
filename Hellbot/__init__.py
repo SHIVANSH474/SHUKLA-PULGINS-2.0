65d0ed5430e890c93f42ea66b38375265c4d7c18
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-01-12 15:49:21 +0530

    Update __init__.py

--- Hellbot/__init__.py
+++ Hellbot/__init__.py
@@ -32,35 +32,6 @@ client = Client(
 )
 
 
-
-from pyrogram import Client
-from config2 import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION
-
-spam_chats = []
-SUDO_USER = SUDO_USERS
-SUDO_USERS.append(OWNER_ID)
-
-app = Client(
-    name="app",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="Hellbot/plugins"),
-    in_memory=True,
-)
-
-client = Client(
-    name="rj",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    session_string=STRING_SESSION,
-    plugins=dict(root="Hellbot/plugins")
-)
-
-
-
-
 START_TIME = time.time()
 
 
